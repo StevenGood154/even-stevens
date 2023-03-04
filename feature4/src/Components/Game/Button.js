76549671f579987ecf51e2ae89957fe949454bf8
StevenGood154 <sgood2@nd.edu>
2023-03-04 14:13:42 -0500

    fully integrated feature 3 into react

--- feature4/src/Components/Game/Button.js
+++ feature4/src/Components/Game/Button.js
@@ -2,7 +2,7 @@
 const Button = ({ buttonText, onAnswerChoiceClick }) => {
   let txt = new DOMParser().parseFromString(buttonText, "text/html");
   let newButtonText = txt.documentElement.textContent;
-  return (<button onClick="{onAnswerChoiceClick}" className="button">
+  return (<button onClick={onAnswerChoiceClick} className="button">
     {newButtonText}
   </button>);
 };
--- feature4/src/Components/Game/Question.js
+++ feature4/src/Components/Game/Question.js
@@ -1,45 +1,26 @@
-import {
-  useRef
-} from "react";
 import Button from "./Button.js";
 
 const Question = ({ data }) => {
   function correctClick() {
     console.log("Correct!");
-    // Will handle correct response later
-    // correctAnswerButtonRef.current.base.classList.add("correctChoice");
   }
   function incorrectClick() {
     console.log("Incorrect");
-    // Will handle incorrect response later
-    // correctAnswerButtonsRef.current.base.classList.add("correctChoice");
-    // incorrectAnswerButtonRefs[0].current.base.classList.add("incorrectChoice");
   }
-
-  const correctAnswerButtonRef = useRef(null);
-  const incorrectAnswerButtonRefs = [useRef(null), useRef(null), useRef(null)];
 
   let txt = new DOMParser().parseFromString(data.get("question"), "text/html");
   let newQuestion = txt.documentElement.textContent;
 
-  // const correctbutton = html`<{Button}
-  //   ref={correctAnswerButton}
-  //   buttonText={data.correct_answer}
-  //   onAnswerChoiceClick="{correctClick}"
-  // />`
-
   const correctAnswerButtonHTML = (<Button
-    ref={correctAnswerButtonRef}
     buttonText={data.get("correct_answer")}
-    onAnswerChoiceClick="{correctClick}"
+    onAnswerChoiceClick={correctClick}
   />);
 
   const incorrectAnswerButtonsHTML = data.get("incorrect_answers").map(
     (incorrect_answer, index) => {
       return (<Button 
-        ref={incorrectAnswerButtonRefs[index]}
         buttonText={incorrect_answer}
-        onAnswerChoiceClick="{incorrectClick}"
+        onAnswerChoiceClick={incorrectClick}
       />);
     }
   );
