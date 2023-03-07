8a14d84673ca3bf8958caa46b9f4342af4a534bd
StevenGood154 <sgood2@nd.edu>
2023-03-07 14:52:32 -0500

    remove unused code

--- feature4/src/Components/Game/Button.js
+++ feature4/src/Components/Game/Button.js
@@ -1,11 +1,11 @@
 import "./Button.css";
 
 // buttonText : what is displayed on button
-const Button = ({ buttonText, onAnswerChoiceClick, buttonRef }) => {
+const Button = ({ buttonText, onAnswerChoiceClick }) => {
   let txt = new DOMParser().parseFromString(buttonText, "text/html");
   let newButtonText = txt.documentElement.textContent;
 
-  return (<button onClick={onAnswerChoiceClick} className="button" ref={buttonRef}>
+  return (<button onClick={onAnswerChoiceClick} className="button">
     {newButtonText}
   </button>);
 };
